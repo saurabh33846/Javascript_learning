0c4b25e1f5e715490943b8dc63f9a5ea8f74341b
Saurabh kumar <43277898+saurabh33846@users.noreply.github.com>
2023-03-11 12:25:45 +0530

    Create thisConcept.js

---
+++ thisConcept.js
@@ -0,0 +1,21 @@
+// Rules of deciding this
+
+// 1. If function is called using new keyword, use the new object created as this.
+// 2. If function is called using call, apply, bind , use that.
+// 3. If function is called via some containging/ owning object, we use that.
+// 4. Default Rule, Global object( except Strict mode)
+// New keyword, Class in JS
+
+function foo() {
+    this.baz = "baz";
+    console.log(this.bar + " " + baz);
+}
+
+var bar = "bar";
+var baz = new foo();
+
+/*
+ Four things happens when we call new in fornt of function, ( Constructor call say)
+ 1. A new object is created.
+ 2. 
+*/
